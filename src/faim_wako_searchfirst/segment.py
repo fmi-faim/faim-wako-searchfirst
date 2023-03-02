9f7fb144ecfe0e39009add544ecfa9552d8042a0
Jan Eglinger <jan.eglinger@fmi.ch>
2023-03-02 11:31:33 +0100

    Major rework of segmentation
    - add filtering for size and eccentricity
    - add grid sampling
    
    (not yet functional: additional_analysis, grid overlap and offset)

--- src/faim_wako_searchfirst/segment.py
+++ src/faim_wako_searchfirst/segment.py
@@ -7,11 +7,15 @@
 import csv
 import json
 import logging
+import re
 from pathlib import Path
-from typing import Union
+from typing import Callable, Union
 
 import confuse
+import numpy as np
+from numpy import ndarray
 from rich.progress import track
+from scipy.ndimage import binary_fill_holes
 from skimage.io import imread
 from skimage.measure import label, regionprops
 
@@ -19,59 +23,178 @@ from skimage.measure import label, regionprops
 def run(folder: Union[str, Path], configfile: str):
     """Analyse first pass of a Wako SearchFirst experiment."""
     # Check if folder_path is valid
-    path = Path(folder)
-    assert path.is_dir(), f"Invalid input folder: {folder}"
+    folder_path = Path(folder)
+    assert folder_path.is_dir(), f"Invalid input folder: {folder}"
 
-    logging.basicConfig(filename=path / (__name__ + ".log"),
+    # Setup logging
+    logging.basicConfig(filename=folder_path / (__name__ + ".log"),
                         format='%(asctime)s - %(name)s - [%(levelname)s] %(message)s',
                         level=logging.INFO)  # encoding="utf-8",
     logger = logging.getLogger(__name__)
 
+    # Read config
     config_path = Path(configfile)
     config = confuse.Configuration("faim-wako-searchfirst")
     config.set_file(config_path, base_for_paths=True)
-    logger.info(f"Reading config parameters from file {config_path.absolute()}.")
-
-    file_selection_params = config["file_selection"].get()
-    segmentation_params = config["segmentation"].get()
-
-    logger.info("Segmentation parameters: " + json.dumps(segmentation_params, indent=4))
 
     # Copy config file to destination
-    config_copy = path / config_path.name
+    config_copy = folder_path / config_path.name
     config_copy.write_text(config.dump())
 
-    segment(path, logger=logger, seg_params=segmentation_params, **file_selection_params)
+    # Segment
+    process(
+        folder_path,
+        file_selection_params=config["file_selection"].get(),
+        segmentation_params=config["segmentation"].get(),
+        additional_analysis_params=config["additional_analysis"].get(),
+        output_params=config["output"].get(),
+        grid_sampling_params=config["grid_sampling"].get(),
+        logger=logger,
+    )
 
 
-def segment(folder, logger, channel, seg_params):
-    """Segment images with the provided segmentation parameters."""
-    logger.info("Starting segmentation...")
-
-    # Filter all TIFs in folder starting with folder's name - and containing channel ID
-    tifs = (sorted(folder.rglob(folder.name + "*" + channel + ".[Tt][Ii][Ff]")))
-
-    # Write CSV file for each TIF
-    for tif in track(tifs):
-        process_file(tif, logger, **seg_params)
-
-    logger.info(f"Finished processing {len(tifs)} image(s).")
+def select_files(
+        folder: Path,
+        channel: str = "C01",
+) -> list[Path]:
+    """Filter all TIFs in folder starting with folder name - and containing channel ID."""
+    return sorted(folder.rglob(folder.name + "*" + channel + ".[Tt][Ii][Ff]"))
 
 
-def process_file(tif, logger, threshold, min_size=0, include_holes=False):
-    """Run segmentation on a single file."""
-    logger.info(f"Opening {tif} ...")
-    img = imread(tif)
+def segment(
+        img,
+        threshold: int,
+        include_holes: bool,
+        min_size: int,
+        max_eccentricity: float,
+):
+    """Segment a given image by global thresholding.
+
+    :param img: input image
+    :param threshold: global threshold
+    :param include_holes: if true, holes will be filled
+    :param min_size: minimum object size
+    :param max_eccentricity: maximum eccentricity of object
+
+    :return: a label image representing the detected objects
+    """
     mask = img > threshold
-    labeled_img, count = label(mask, return_num=True)
+    if include_holes:
+        mask = binary_fill_holes(mask)
+    labeled_image = label(mask)
+    regions = regionprops(labeled_image)
+    for region in regions:
+        if region.area < min_size or region.eccentricity > max_eccentricity:
+            labeled_image[labeled_image == region.label] = 0
+    return labeled_image
+
+
+def segment_file(
+        tif: str,
+        segment_fn: Callable,
+        **kwargs,
+):
+    """Segment a tif file using a provided segmentation function."""
+    img = imread(tif)
+    labeled_image = segment_fn(img, **kwargs)
+    return img, labeled_image
+
+
+def filter_objects_by_intensity(labels, img, min_intensity):
+    """Filter objects in 'labels' by intensity in 'img'."""
+    # TODO implement object filtering
+    return labels
+
+
+def sample_grid(labeled_img: ndarray, path, mag_first_pass, mag_second_pass, overlap_percent,
+                offset_grid_origin_percent):
+    """Save grid positions of the tiles that contain objects."""
+    factor = mag_first_pass / mag_second_pass
+    tile_size_y = labeled_img.shape[0] * factor
+    tile_size_x = labeled_img.shape[1] * factor
+
+    with open(path, "w", newline="") as csv_file:
+        c = csv.writer(csv_file)
+        count = 0
+        for y in np.arange(0, labeled_img.shape[0], tile_size_y):
+            for x in np.arange(0, labeled_img.shape[1], tile_size_x):
+                if np.max(
+                        labeled_img[
+                        int(np.floor(y)):int(np.ceil(y + tile_size_y)),
+                        int(np.floor(x)):int(np.ceil(x + tile_size_x))
+                        ]
+                ) > 0:
+                    c.writerow([count, x + tile_size_x / 2, y + tile_size_y / 2])
+                    count += 1
+
+
+def report_center_coordinates(labeled_img, path):
+    """Save center position of each object in 'labeled_img'."""
     regions = regionprops(labeled_img)
-
-    # TODO implement size and eccentricity filtering
-    # TODO create segmentation preview
-    # TODO implement object <-> grid sampling
-
-    logger.info(f"Found {len(regions)} regions.")
-    with open(tif.parent / (tif.stem + ".csv"), "w", newline="") as csv_file:
+    with open(path, "w", newline="") as csv_file:
         c = csv.writer(csv_file)
         for region in regions:
             c.writerow([region.label, *region.centroid])
+
+
+def get_other_channel_file(tif_file: Path, target_channel: str) -> Path:
+    """Detect the file of target channel with the same well and field as the given 'tif_file'."""
+    pattern = re.compile(r"(.*_[A-Z]\d{2}_T\d{4}F\d{3}L\d{2})(A\d{2})(Z\d{2})(C\d{2})\.tif")
+    m = pattern.fullmatch(tif_file.name)
+    assert m is not None
+    candidate_files = tif_file.parent.glob("*" + target_channel + ".[Tt][Ii][Ff]")
+    for candidate in candidate_files:
+        n = pattern.fullmatch(candidate.name)
+        if (n is not None) and (n.group(4) == target_channel) and (m.group(1) == n.group(1)):
+            logging.info(f"Found file {candidate.name}.")
+            return candidate
+    raise FileNotFoundError(f"No matching file for channel {target_channel}.")
+
+
+def additional_analysis(
+        tif_file, labels, filter_fn, enabled=False, target_channel=None, min_intensity=None
+):
+    """Filter objects in 'labels' using the provided function."""
+    if not enabled:
+        return labels
+    intensity_image = imread(get_other_channel_file(tif_file, target_channel))
+    return filter_fn(labels, intensity_image, min_intensity)
+
+
+def process(
+        folder: Path,
+        file_selection_params: dict,
+        segmentation_params: dict,
+        additional_analysis_params: dict,
+        output_params: dict,
+        grid_sampling_params: dict,
+        logger=logging,
+) -> None:
+    """Segment images with the provided segmentation parameters."""
+    logger.info("Segmentation parameters: " + json.dumps(segmentation_params, indent=4))
+
+    tif_files = select_files(folder=folder, **file_selection_params)
+
+    # Write CSV file for each TIF
+    for tif_file in track(tif_files):
+        # file -> segmentation mask and image
+        img, labels = segment_file(tif_file, segment, **segmentation_params)
+        logger.info("include_holes: " + str(segmentation_params["include_holes"]))
+
+        # addition analysis (e.g. filter by intensity in other channel)
+        labels = additional_analysis(
+            tif_file, labels, filter_objects_by_intensity,
+            **additional_analysis_params
+        )
+
+        # mask -> csv
+        csv_path = tif_file.parent / (tif_file.stem + ".csv")
+        if output_params["type"] == "grid":
+            sample_grid(labels, csv_path, **grid_sampling_params)
+        else:
+            report_center_coordinates(labels, csv_path)
+
+        # mask + image -> preview
+        # TODO implement preview rendering
+
+    logger.info(f"Finished processing {len(tif_files)} image(s).")
--- tests/test_segment.py
+++ tests/test_segment.py
@@ -3,16 +3,105 @@
 # SPDX-License-Identifier: MIT
 
 """Test 'faim_wako_searchfirst.segment' module."""
+import csv
+from pathlib import Path
+
 import pytest
-from faim_wako_searchfirst.segment import run
+from faim_wako_searchfirst.segment import process, run
 
 
-def test_segment_test_set():
+@pytest.fixture()
+def _data_path():
+    return Path("tests/resources/TestSet")
+
+
+@pytest.fixture(autouse=True)
+def _clean_up(_data_path):
+    yield  # Run tests
+    # Remove all .csv and .yml from test_set_path
+    for f in _data_path.glob("*.csv"):
+        f.unlink()
+    for f in _data_path.glob("*.yml"):
+        f.unlink()
+
+
+def test_run_test_set(_data_path):
     """Run a segmentation on the test set."""
-    run("tests/resources/TestSet", configfile="config.yml")
+    run(_data_path, configfile="config.yml")
+    csv_path = _data_path / "TestSet_D07_T0001F002L01A02Z01C01.csv"
+    assert csv_path.exists()
+    # TODO assert csv data correct
+    with open(csv_path, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        assert sum(1 for _ in reader) == 12, "Incorrect number of objects detected."
 
 
-def test_segment_invalid_folder():
-    """Test error on non-existing folder."""
+def test_process_test_set(_data_path):
+    """Test process() with defined parameters."""
+    file_selection_params = {
+        "channel": "C01",
+    }
+    segmentation_params = {
+        "threshold": 1,
+        "include_holes": False,
+        "min_size": 10,
+        "max_eccentricity": 0.9,
+    }
+    additional_analysis_params = {
+        "enabled": False,
+    }
+    output_params = {
+        "type": "centers",
+    }
+    grid_sampling_params = {}
+
+    process(
+        _data_path,
+        file_selection_params,
+        segmentation_params,
+        additional_analysis_params,
+        output_params,
+        grid_sampling_params,
+    )
+
+    csv_path = _data_path / "TestSet_D07_T0001F002L01A02Z01C01.csv"
+    assert csv_path.exists()
+    # TODO assert csv data correct
+    with open(csv_path, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        assert sum(1 for _ in reader) == 5, "Incorrect number of objects detected."
+
+
+def test_run_invalid_folder():
+    """Test AssertionError on non-existing folder."""
     with pytest.raises(AssertionError):
         run("/some/invalid/folder", configfile="config.yml")
+
+
+def test_process_invalid_second_channel(_data_path):
+    """Test FileNotFoundError on invalid target channel."""
+    file_selection_params = {
+        "channel": "C01",
+    }
+    segmentation_params = {
+        "threshold": 1,
+        "include_holes": False,
+        "min_size": 10,
+        "max_eccentricity": 0.9,
+    }
+    additional_analysis_params = {
+        "enabled": True,
+        "target_channel": "C04",
+    }
+    output_params = {}
+    grid_sampling_params = {}
+
+    with pytest.raises(FileNotFoundError):
+        process(
+            _data_path,
+            file_selection_params,
+            segmentation_params,
+            additional_analysis_params,
+            output_params,
+            grid_sampling_params,
+        )
