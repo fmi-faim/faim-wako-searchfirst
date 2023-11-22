38bbd0709dce37f14ff61d63b442b6dd03d72613
Jan Eglinger <jan.eglinger@fmi.ch>
2023-11-22 11:10:19 +0100

    Add cellpose segmentation
    Also update supported Python versions (cellpose needs 3.8+).

--- src/faim_wako_searchfirst/segment.py
+++ src/faim_wako_searchfirst/segment.py
@@ -9,8 +9,11 @@ and must return a label image.
 """
 
 import logging
+from pathlib import Path
+from typing import Union
 
 import numpy as np
+from cellpose import models
 from scipy.ndimage import binary_fill_holes
 from skimage.measure import label, regionprops
 
@@ -26,10 +29,6 @@ def threshold(
     :param img: input image
     :param threshold: global threshold
     :param include_holes: if true, holes will be filled
-    :param min_size: minimum object size
-    :param max_size: maximum object size
-    :param min_eccentricity: minimum eccentricity of object
-    :param max_eccentricity: maximum eccentricity of object
     :param logger:
 
     :return: a label image representing the detected objects
@@ -41,3 +40,32 @@ def threshold(
     regions = regionprops(labeled_image)
     logger.info(f"Found {len(regions)} connected components.")
     return labeled_image
+
+
+def cellpose(
+    img,
+    diameter: float,
+    pretrained_model: Union[str, Path] = "cyto2",
+    logger=logging,
+    **kwargs,
+):
+    """Segment a given image by global thresholding.
+
+    :param img: input image
+    :param diameter: expected object diameter
+    :param pretrained_model: name of cellpose model, or path to pretrained model
+    :param logger:
+
+    :return: a label image representing the detected objects
+    """
+    logger.info(f"Load cellpose model: {pretrained_model}")
+    model: models.CellposeModel = models.CellposeModel(
+        pretrained_model=pretrained_model,
+    )
+    mask, _, _ = model.eval(
+        img,
+        channels=[0, 0],
+        diameter=diameter,
+        **kwargs,
+    )
+    return mask
